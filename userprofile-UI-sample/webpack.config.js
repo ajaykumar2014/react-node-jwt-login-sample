accbd6fd084cf32c276ce82174290ac2f4022a4f
ajagupta <akumar298@gmail.com>
2020-01-02 14:32:01 +0530

    added UI

--- userprofile-UI-sample/webpack.config.js
+++ userprofile-UI-sample/webpack.config.js
@@ -1,28 +1,61 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader'
-            }
+  mode: "development",
+  entry: "./src/index.js",
+  resolve: {
+    modules: [
+      path.resolve(__dirname, "node_modules"),
+      path.resolve(__dirname, "./src/")
+    ],
+    extensions: [".js", ".jsx", ".css", ".jpg", ".png"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [path.resolve(__dirname, "./src")],
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {}
+          }
         ]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html'
-    })],
-    devServer: {
-        historyApiFallback: true
-    },
-    externals: {
-        // global app config object
-        config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
-        })
-    }
-}
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"), // change this
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      filename: "index.html"
+    })
+  ],
+  devServer: {
+    contentBase: "./build",
+    historyApiFallback: true,
+    host: "0.0.0.0",
+    port: 8080
+  },
+  externals: {
+    config: JSON.stringify({
+      apiUrl: "http://localhost:3000"
+    })
+  }
+};
